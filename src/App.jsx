5bcb35c0d1dd5c06aad6f4a68a249bbfe78a33a8
kocpeel <134693584+kocpeel@users.noreply.github.com>
2024-10-24 11:19:29 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.scss";
-import "./components/Blob/Blob";
 
 function App() {
   const [data, setData] = useState(null);
